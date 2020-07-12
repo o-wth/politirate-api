bd0a892ea8a50fdcc6af956cf0b6cdfaded3fc9d
Sumanth Ratna <sumanthratna@gmail.com>
2020-07-12 18:38:43 -0400

    Remove first and last names from Politician model
    Human names are diverse and there's no way to truly parse names without the risk of being incorrect. We can add firstName, lastName, prefix, and suffix if we're able to extract this information from politicians' social medias.

--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,12 @@
 pub struct Politician {
-  pub firstName: String,
-  pub lastName: String,
-  pub fullName, String,
+  pub name, String,
   pub phoneNumber: String,
   pub party: String
 }
 
 impl Politician {
-  pub fn firstName(&self) -> String {
-    self.firstName
-  }
-  pub fn lastName(&self) -> String {
-    self.lastName
-  }
-  pub fn fullName(&self) -> String {
-    self.fullName
+  pub fn name(&self) -> String {
+    self.name
   }
   pub fn phoneNumber(&self) -> String {
     self.phoneNumber
